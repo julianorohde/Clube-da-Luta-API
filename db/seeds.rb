12b55aed1929eb4e555ea4ad058c606774c29e1d
JULIANO ROHDE <julianorohde@hotmail.com>
2023-03-21 18:23:41 -0400

    Add Faker gem to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Fighter.delete_all
+Power.delete_all
+
+10.times do
+  Fighter.create(name: Faker::Games::StreetFighter.character, health: Faker::Number.between(from: 500, to: 1000),
+                 attack_power: Faker::Number.between(from: 1, to: 20), defense_power: Faker::Number.between(from: 1, to: 20))
+end
+
+5.times do
+  Power.create(name: Faker::Games::StreetFighter.move, damage: Faker::Number.between(from: 1, to: 20),
+               cooldown: Faker::Number.between(from: 5, to: 60))
+end
